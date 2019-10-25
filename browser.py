ea198e44df28da20ccb4931a59e4573f4e9d482b
Filip Mularski <filipelblag@gmail.com>
2019-10-25 20:07:27 +0200

    getting submissions info

--- browser.py
+++ browser.py
@@ -1,10 +1,16 @@
 import praw
 import login
 
-client_id = 'gMuenas4Y-YSDA'
-client_secret = 'KUjFTaUkYH8yRJDjr9Ea8IBYNJ0'
+client_id = '37vflv4sf4SxNA'
+client_secret = 'U9dY8AHct7LgzmiCEfPvymbyr8Y'
 
-reddit = praw.Reddit(client_id, client_secret, username=login.username,
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, username=login.username,
                      password=login.password, user_agent='reddit_browser')
 
+subreddit = reddit.subreddit('DragonBallLegends')
 
+hot_dblegends = subreddit.hot(limit=5)
+
+for submission in hot_dblegends:
+    if not submission.stickied:     # pinned submissions
+        print(f'{submission.title} Up: {submission.ups} Down: {submission.downs} Visited: {submission.visited}')
